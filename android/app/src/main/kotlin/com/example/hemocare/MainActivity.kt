0d79871b81a0ebacde387e299553393a41d49fb2
Alapan10008 <alapansen@kgpian.iitkgp.ac.in>
2024-02-16 19:07:12 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/hemocare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hemocare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// Colors
+
+const kBackgroundColor =  Color.fromARGB(255, 232, 173, 173);
+const kPrimaryColor = Color(0xFF2D5D70);
+const kSecondaryColor = Color(0xFFE1485F);
